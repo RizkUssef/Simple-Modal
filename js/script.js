60d17a277478d9e69199be5e3550d253a0952356
Rizk Ussef <rizkusgt67@gmail.com>
2024-11-14 16:36:46 +0200

    handle the show and colse the modal and next & prev btns

---
+++ js/script.js
@@ -0,0 +1,83 @@
+let imgs = Array.from(document.querySelectorAll(".p-0 .img-container img"));
+let modal = document.getElementById("modal");
+let modal_contianer = document.getElementById("modal-contianer");
+let modal_img = document.getElementById("modal-img");
+let close =document.getElementById("close");
+let next =document.getElementById("next");
+let prev =document.getElementById("prev");
+let index =0;
+openModal();
+closeModal();
+nextImg();
+prevImg();
+document.addEventListener("keydown",function (e){
+    console.log(e.key);
+    // if(e.key == ""){
+
+    // }
+    
+})
+function openModal(){
+    for (let i = 0; i < imgs.length; i++) {
+        let src = imgs[i].getAttribute("src");
+        imgs[i].addEventListener("click", function() {
+            showModal();
+            setNewSrc(src);
+            index=i;
+        });
+    }
+};
+function closeModal() {
+    close.addEventListener("click", function() {
+        hideModal();
+    });
+    document.addEventListener("keydown", function(e) {
+        if(e.key == "Escape") {
+            hideModal();
+        }
+    });
+    modal_contianer.addEventListener("click", function(e) {
+        if(modal_contianer == e.target) {
+            hideModal();
+            e.stopPropagation();
+        }
+    })
+}
+
+function nextImg(){
+    function handle(){
+        index++;
+        index=index%(imgs.length);
+        setNewSrc(imgs[index].getAttribute("src"));
+    }
+    next.addEventListener("click",function(){
+        handle();
+    });
+    document.addEventListener("keydown",function (e){
+        if(e.key == "ArrowRight") handle();
+    })
+}
+function prevImg(){
+    function handle(){
+        index--;
+        if(index < 0) {
+            index  = imgs.length-1;
+        }
+        setNewSrc(imgs[index].getAttribute("src"));
+    }
+    prev.addEventListener("click",function(){
+        handle();
+    })
+    document.addEventListener("keydown",function(e){
+        if(e.key=="ArrowLeft") handle();
+    });
+}
+function setNewSrc(src) {
+    modal_img.setAttribute("src", src);
+}
+function showModal(){
+    modal.style.display = "block";
+}
+function hideModal(){
+    modal.style.display = "none";
+}
